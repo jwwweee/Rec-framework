7307b1fbcbe60aa9e86aaab22fa40a4e8d5ea5c6
Wei Jiang <40032291+jwwweee@users.noreply.github.com>
2023-02-27 19:10:18 +1000

    update Rec class

--- Rec.py
+++ Rec.py
@@ -11,12 +11,18 @@ from util.evaluator import *
 
 
 class Rec(object):
-    def __init__(self, name_data, name_model, is_social=False, task_type='ranking') -> None:
+    def __init__(self, name_data, name_model, K=10, is_social=False, task_type='ranking') -> None:
         self.name_model = name_model
         self.is_social = is_social
         self.task_type = task_type
         self.EPOCH = 5000
+        self.K = K
+        self.early_stopping_counter = 0
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
+
+        self.data = Data(data_path='data/', name_data=name_data, is_social=True) # get data
+
+        self.train_set, self.valid_set, self.test_set = self.data.split_dataset()
         
         print('Initializing...')
         # ----------------- initialze graphs -----------------
@@ -26,8 +32,8 @@ class Rec(object):
         if os.path.exists(interact_graph_path):
             self.sparse_interact_graph = sp.load_npz(interact_graph_path)
         else:
-            interact_graph = data.get_interact_graph()
-            self.sparse_interact_graph = data.get_sparse_graph(interact_graph, graph_type='interact')
+            interact_graph = self.data.get_interact_graph()
+            self.sparse_interact_graph = self.data.get_sparse_graph(interact_graph, graph_type='interact')
             sp.save_npz(interact_graph_path, self.sparse_interact_graph)
 
         # initialize social graph if "is_social" is true
@@ -37,28 +43,74 @@ class Rec(object):
             if os.path.exists(social_graph_path):
                 self.sparse_social_graph = sp.load_npz(social_graph_path)
             else:
-                social_graph = data.get_social_graph()
-                self.sparse_social_graph = data.get_sparse_graph(social_graph, graph_type='social')
+                social_graph = self.data.get_social_graph()
+                self.sparse_social_graph = self.data.get_sparse_graph(social_graph, graph_type='social')
                 sp.save_npz(social_graph_path, self.sparse_social_graph)
 
         # ----------------- initial model -----------------
         self.model_path = 'model/pt/DiffNet.pt'
-        self.model = DiffNet(data.num_users, data.num_items, args)
+        self.model = DiffNet(self.data.num_users, self.data.num_items, args)
 
         if self.is_social:
             self.model.initialize_graph(self.sparse_interact_graph, self.sparse_social_graph)
         else:
             self.model.initialize_graph(self.sparse_interact_graph)
 
-    def train(self, stop_metric_type='recall'):
+    def _ealry_stop(self, valid_results, stop_metric_type):
         """
         """
+        stop_metric = valid_results[stop_metric_type]
+
+        if self.task_type == 'ranking':
+            if stop_metric > best_metric:
+                best_metric = stop_metric
+                self.early_stopping_counter = 0 # restart counter
+                
+                print("Saving model...")
+                torch.save(self.model, self.model_path) # save model in terms of validation result
+            else:
+                self.early_stopping_counter += 1
+        else: # rating
+            if stop_metric < best_metric:
+                best_metric = stop_metric
+                self.early_stopping_counter = 0 # restart counter
+                
+                print("Saving model...")
+                torch.save(self.model, self.model_path) # save model in terms of validation result
+            else:
+                self.early_stopping_counter += 1
+
+    def _validate(self, epoch, evaluator_valid):
+        """
+        """
+        valid_start_time = time()
+        
+        valid_results = evaluator_valid.evaluate(self.model)
+        
+        valid_finish_time = time()
+        
+        valid_stat = 'Epoch %d [%.1fs]: recall=[%.5f], ' \
+                'precision=[%.5f], hit=[%.5f], ndcg=[%.5f]' % \
+                (epoch, valid_finish_time - valid_start_time, valid_results['recall'],
+                    valid_results['precision'], valid_results['hit_ratio'],
+                    valid_results['ndcg'])
+        print(valid_stat)
+
+        return valid_results
+
+    def train(self, train_set, valid_set, stop_metric_type='recall'):
+        """
+        """
+
+        train_set = self.data.retrieve_user_interacts(train_set)
+        valid_set = self.data.retrieve_user_interacts(valid_set)
+        
         # ----------------- Train -----------------
         # initialize optimizer
         optimizer = optim.Adam(self.model.parameters(), lr=args.lr)
         
         # initial evaluator for validation
-        evaluator_valid = Evaluator(valid_set_dict, args.K, data.num_items, args.batch_size)
+        evaluator_valid = Evaluator(valid_set, self.K, self.data.num_items, args.batch_size)
         
         if self.task_type == 'ranking':
             best_metric = 0
@@ -69,10 +121,10 @@ class Rec(object):
             epoch_time = time()
             loss = 0.
 
-            num_train_batch = data.num_train // args.batch_size + 1
+            num_train_batch = self.data.num_train // args.batch_size + 1
 
             for idx in range(num_train_batch):
-                users, pos_items, neg_items = data.pair_data_sampling(train_set_dict, args.batch_size)
+                users, pos_items, neg_items = self.data.pair_data_sampling(train_set, args.batch_size)
                 user_final_embeddings, pos_item_final_embeddings, neg_item_final_embeddings = self.model(users,
                                                                             pos_items,
                                                                             neg_items)
@@ -91,50 +143,20 @@ class Rec(object):
 
             # ----------------- Validation -----------------
             if (epoch + 1) % 10 == 0:
-                valid_start_time = time()
-                
-                valid_results = evaluator_valid.evaluate(self.model)
-                
-                valid_finish_time = time()
-                
-                valid_stat = 'Epoch %d [%.1fs]: traininig loss==[%.5f], recall=[%.5f], ' \
-                        'precision=[%.5f], hit=[%.5f], ndcg=[%.5f]' % \
-                        (epoch, valid_finish_time - valid_start_time, loss, valid_results['recall'],
-                            valid_results['precision'], valid_results['hit_ratio'],
-                            valid_results['ndcg'])
-                print(valid_stat)
+                valid_results = self._validate(epoch, evaluator_valid)
         
                 # ----------------- Early stopping -----------------
-                
-                stop_metric = valid_results[stop_metric_type]
+                self._ealry_stop(valid_results, stop_metric_type)
 
-                if self.task_type == 'ranking':
-                    if stop_metric > best_metric:
-                        best_metric = stop_metric
-                        early_stopping_counter = 0 # restart counter
-                        
-                        print("Saving model...")
-                        torch.save(self.model, self.model_path) # save model in terms of validation result
-                    else:
-                        early_stopping_counter += 1
-                else: # rating
-                    if stop_metric < best_metric:
-                        best_metric = stop_metric
-                        early_stopping_counter = 0 # restart counter
-                        
-                        print("Saving model...")
-                        torch.save(self.model, self.model_path) # save model in terms of validation result
-                    else:
-                        early_stopping_counter += 1
-
-                if early_stopping_counter > 50: # validate 50 times (500 epoches), if the best metric is not updated, then early stop
+                if self.early_stopping_counter > 50: # validate 50 times (500 epoches), if the best metric is not updated, then early stop
                     print("Early stopping...")
                     break
-    def test(self):
+            
+    def test(self, test_set_dict):
         """
         """
         # ----------------- Test -----------------
-        evaluator_test = Evaluator(test_set_dict, args.K, data.num_items, args.batch_size)
+        evaluator_test = Evaluator(test_set_dict, args.K, self.data.num_items, args.batch_size)
 
         model = torch.load(self.model_path) # load model
 
@@ -147,25 +169,7 @@ class Rec(object):
                             test_results['ndcg'])
         print(test_stat)
 
-if __name__ == '__main__':
 
-    print('Loading data...')
-    prepare_start_time = time()
-    data = Data(data_path='data/', name_data=args.dataset, is_social=True) # get data
-
-    train_set, valid_set, test_set = data.split_dataset()
-    
-    train_set_dict = data.retrieve_user_interacts(train_set)
-    valid_set_dict = data.retrieve_user_interacts(valid_set)
-    test_set_dict = data.retrieve_user_interacts(test_set)
-
-    data.print_statistics()
-    
-    train_start_time = time()
-    
-    prepare_state = 'Preparation compeleted [%.1fs]' % (train_start_time - prepare_start_time)
-    print(prepare_state)
-    print('Start training...')
 
 
 
